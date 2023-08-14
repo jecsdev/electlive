d2827e29eb8269e210a0b7b43a650351606a1614
john0 <jcampusano@k3d.do>
2023-08-14 09:30:11 -0400

    fix error on ViewModelModule

--- app/src/main/java/com/jecsdev/eleclive/core/di/ViewModelModule.kt
+++ app/src/main/java/com/jecsdev/eleclive/core/di/ViewModelModule.kt
@@ -1,6 +1,6 @@
 package com.jecsdev.eleclive.core.di
 
-import com.jecsdev.eleclive.core.network.ApiService
+import com.jecsdev.eleclive.data.domain.Repository.Repository
 import com.jecsdev.eleclive.data.domain.UseCases.GetElectionsUseCase
 import dagger.Module
 import dagger.Provides
@@ -11,7 +11,7 @@ import dagger.hilt.android.components.ViewModelComponent
 @InstallIn(ViewModelComponent::class)
 class ViewModelModule {
     @Provides
-    fun provideGetElectionsUseCase(apiService: ApiService): GetElectionsUseCase {
-        return GetElectionsUseCase(apiService)
+    fun provideGetElectionsUseCase(repository: Repository): GetElectionsUseCase {
+        return GetElectionsUseCase(repository)
     }
 }
