aaa8a8b3ea9906a940d1c3cf04aee1c4b02b9de1
john0 <jcampusano@k3d.do>
2023-08-17 00:22:52 -0400

    Add fields to card

--- app/src/main/java/com/jecsdev/eleclive/ui/views/components/ElectionCard.kt
+++ app/src/main/java/com/jecsdev/eleclive/ui/views/components/ElectionCard.kt
@@ -1,18 +1,51 @@
 package com.jecsdev.eleclive.ui.views.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jecsdev.eleclive.R
 
+@Preview(showSystemUi = true)
 @Composable
 fun ElectionCard() {
     Card(
-        modifier = Modifier.fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        elevation = CardDefaults.elevatedCardElevation(16.dp),
     ) {
-
+        Column(Modifier.padding(12.dp)) {
+            Text(
+                text = stringResource(R.string.election_name),
+                modifier = Modifier.height(IntrinsicSize.Min),
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(Modifier.height(4.dp))
+            Text(
+                text = stringResource(R.string.election_date),
+                modifier = Modifier.height(IntrinsicSize.Min)
+            )
+            Spacer(Modifier.height(4.dp))
+            Text(
+                text = stringResource(R.string.election_description),
+                modifier = Modifier.height(IntrinsicSize.Min)
+            )
+        }
     }
 }
