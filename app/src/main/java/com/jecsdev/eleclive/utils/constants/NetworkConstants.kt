e36173ac6d6cc93cb0ad721822ff4ef51cb933cb
Jecs <john024996@gmail.com>
2023-12-29 12:35:39 -0400

    add new certificate

--- app/src/main/java/com/jecsdev/eleclive/utils/constants/NetworkConstants.kt
+++ app/src/main/java/com/jecsdev/eleclive/utils/constants/NetworkConstants.kt
@@ -5,7 +5,7 @@ package com.jecsdev.eleclive.utils.constants
  */
 object NetworkConstants {
     // Connections constants
-    const val BASEURL = "https://10.0.0.3:45455/"
+    const val BASEURL = "https://10.0.0.48:45455/"
     const val RESPONSE = "Response"
 
 
